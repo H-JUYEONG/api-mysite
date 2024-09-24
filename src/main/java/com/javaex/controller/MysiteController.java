c7a87abfd03cd4d824741b84e6542b84338fb760
H-JUYEONG <dud9902@gmail.com>
2024-09-24 18:04:00 +0900

    z

--- src/main/java/com/javaex/controller/MysiteController.java
+++ src/main/java/com/javaex/controller/MysiteController.java
@@ -1,12 +1,32 @@
 package com.javaex.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.javaex.service.UserService;
+import com.javaex.vo.UserVo;
 
 @RestController
 @Controller
 public class MysiteController {
 	
+	@Autowired
+	private UserService userService;
 	
+	/* 회원가입 */
+	@PostMapping("/api/users")
+	public int join(@RequestBody UserVo userVo) {
+		System.out.println("UserController.join()");
+
+		int count = userService.exeJoinUser(userVo);
+
+		return count;
+	}
 
 }
